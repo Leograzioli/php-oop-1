2f49048255292f07ad0acc7233e1f262bbd9a133
Leograzioli <leograzioli@gmail.com>
2022-12-07 15:08:28 +0100

    bonus 1

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ class Movie {
     public $genre;
     public $description;
 
-    function __construct($title, $duration, $genre, $description)
+    function __construct(string $title, string $duration, array $genre, string $description)
     {
         $this->title = $title;
         $this->duration = $duration;
@@ -15,10 +15,20 @@ class Movie {
     }
 
     public function print() {
-        return "Movie Title: $this->title; Genre: $this->genre; Duration: $this->duration; Description: $this->description;";
+        return "Movie Title: $this->title Genre: " . $this->getGenres() . "Duration: $this->duration; Description: $this->description;";
+    }
+
+    public function getGenres() {
+        $genres = "";
+        foreach ($this->genre as $item) {
+            $genres .= $item . " ";
+        }
+        return $genres;
     }
 }
 
-$my_movie = new Movie("codice da vinci", "120 min", "action", "lorem ipsum tua sorella");
+$my_movie = [new Movie("codice da vinci", "120 min", ["action", "comedy"], "lorem ipsum tua sorella"), new Movie("Le avventure di francesco e phyton", "120 min", ["comedy"], "lorem ipsum tua sorella")];
 
-echo $my_movie->print();
+foreach($my_movie as $movie) {
+    echo $movie->print() . "<br>";
+}
